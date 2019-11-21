bdcb028362b03174ebdd5cc67de9ba192d731a5b
Lin <1731193812@qq.com>
2019-11-21 16:34:15 +0800

    modify arrayHelper.js

--- asset/js/arrayHelper.js
+++ asset/js/arrayHelper.js
@@ -5,8 +5,8 @@
 
 /**
  * @method json对象数组对同key的value求和
- * @param array
- * @param param
+ * @param array 传入的数组
+ * @param param 不定项字段（key）
  * @return 对应key的json对象
  */
 function sumSameKeysInObj(array, ...param) {
@@ -33,28 +33,5 @@ function sumSameKeysInObj(array, ...param) {
         }
     });
     return temp;
-};
+}
 
-var jsonArr = [
-    {
-        serviceTime: [15370, 169397, 34197, 8541, 49074, 387504, 1492682, 168966, 158662, 109246, 1208830, 255854, 49970, 483, 17183],
-        volunteerActivityNum: [4396, 3875, 1584, 1023, 2934, 5525, 6152, 1843, 4002, 5694, 6244, 4516, 1727, 77, 474],
-        volunteersNum: [5859, 34159, 44420, 14362, 58139, 87597, 139746, 17896, 55975, 25204, 41793, 43206, 42835, 225, 13959],
-        totalServiceTime: "1160772281",
-        totalVolunteerActivityNum: 49691,
-        totalVolunteers: 625523
-    },
-    {
-        serviceTime: [15370, 169397, 34197, 8541, 49074, 387504, 1492682, 168966, 158662, 109246, 1208830, 255854, 49970, 483, 17183],
-        volunteerActivityNum: [4396, 3875, 1584, 1023, 2934, 5525, 6152, 1843, 4002, 5694, 6244, 4516, 1727, 77, 474],
-        volunteersNum: [5859, 34159, 44420, 14362, 58139, 87597, 139746, 17896, 55975, 25204, 41793, 43206, 42835, 225, 13959],
-        totalServiceTime: "1160772281",
-        totalVolunteerActivityNum: 49691,
-        totalVolunteers: 625523
-    },
-];
-
-var dataInProvince = sumSameKeysInObj(jsonArr, 'serviceTime','totalServiceTime');
-
-console.log(dataInProvince)
-console.log(jsonArr)
